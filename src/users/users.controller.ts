d59390b0ab45ab528e89ad3e1fb8b5bf31db54ff
BillStephens2022 <two4onebill@yahoo.com>
2024-06-17 07:03:25 -0400

    created a user dto to ensure only certain user fields are exposed when queried (i.e. no password revealed)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,9 +2,11 @@ import { Controller, Post, Body, Get, Patch, Delete, Param, Query, NotFoundExcep
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize-interceptor';
+import { Serialize } from 'src/interceptors/serialize-interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -13,10 +15,8 @@ export class UsersController {
       this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
-      console.log('handler is running');
       const user = await this.usersService.findOne(parseInt(id));
       if (!user) {
         throw new NotFoundException('user not found');
