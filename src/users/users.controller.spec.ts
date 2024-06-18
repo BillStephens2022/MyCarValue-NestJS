bca94244bb5ee468b0a01b2a76724dfa4ebe5129
BillStephens2022 <two4onebill@yahoo.com>
2024-06-18 09:54:14 -0400

    added tests for finding a user by id, finding all users with a given email

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
@@ -39,4 +40,21 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('foundOne@email.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('foundOne@email.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
 });
